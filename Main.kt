50349d62f3592350831b4711471442230b6c900d
Alphabeater <19984045+Alphabeater@users.noreply.github.com>
2020-11-01 10:29:01 -0300

    Stage 2/5: Let’s get metric

--- Main.kt
+++ Main.kt
@@ -1,11 +1,10 @@
 package converter
+import java.util.*
+
+val s = Scanner(System.`in`)
 
 fun main() {
-    println("""
-        145 centimeters is 1.45 meters
-        2 miles is 3.2187 kilometers
-        5.5 inches is 139.7 millimeters
-        12 degrees Celsius is 53.6 degrees Fahrenheit
-        3 pounds is 1.360776 kilograms
-    """.trimIndent())
+    print("Enter a number of kilometers: ")
+    val distance = s.nextInt()
+    println("$distance kilometers is ${distance * 1000} meters")
 }
